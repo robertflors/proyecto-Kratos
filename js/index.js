8d7c11842ccd24dd2aababe34a809a2760e8f22b
Robert Flores <rrfloresgonzalez@gmail.com>
2021-05-13 22:40:38 -0300

    se están trabajando en los detalles finales

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// para darle el focus al enlace de home en el navbar cuando cargue la página
+$(document).ready(function () {
+    $("#enlaceHome").focus();
+});
+// para darle el focus al enlace que se haya seleccionado
+// inicio
+$("#enlaceNosotros").click(function () {
+    $(document).ready(function () {
+        $("#enlaceNosotros").focus();
+    });
+});
+
+$("#enlaceServicios").click(function () {
+    $(document).ready(function () {
+        $("#enlaceServicios").focus();
+    });
+});
+
+$("#enlaceArticulos").click(function () {
+    $(document).ready(function () {
+        $("#enlaceArticulos").focus();
+    });
+});
+
+$("#enlaceContacto").click(function () {
+    $(document).ready(function () {
+        $("#enlaceContacto").focus();
+    });
+});
+// fin
+
+// para cambiar el color del navbar al hacer scroll
+$(window).scroll(function () {
+    if ($("#menu").offset().top > 150) {
+        $("#menu").addClass("navbar--azul");
+    } else {
+        $("#menu").removeClass("navbar--azul");
+    }
+});
+
+// para cambiar el color del navbar al darle click al toggler responsivo
+$(".navbar-toggler").click(function () { 
+    $("#menu").css("background-color", "rgba(22, 41, 84, 0.9)");
+    $("#navbarSupportedContent").toggle("fast");  
+});
+
+// para cerrar el toggler responsivo al seleccionar un enlace
+$(".nav-link").click(function () { 
+    $("#navbarSupportedContent").hide("fast");   
+});
---
+++ js/scrollAnimacion.js
@@ -0,0 +1,54 @@
+window.sr = ScrollReveal();
+// -----------------index---------------------------
+sr.reveal('#menu__home', {
+
+        duration: 1900,
+        origin: 'top',
+        distance: '200px'
+
+    });
+
+sr.reveal('.main__titulo', {
+
+        duration: 5000,
+        origin: 'left',
+        distance: '800px'
+
+    });
+
+sr.reveal('.main__logoTransicion', {
+
+        duration: 4000,
+        origin: 'rigth',
+        distance: '800px'
+
+    });
+
+// -----------------servicios------------------------
+sr.reveal('.h2__servicios', {
+
+        duration: 1000,
+        origin: 'right',
+        distance: '300px'
+
+    });    
+
+sr.reveal('.h1__servicios', {
+
+        duration: 2500,
+        origin: 'top',
+        distance: '300px'
+
+    }); 
+
+sr.reveal('.h3__servicios', {
+
+        duration: 1000,
+        origin: 'left',
+        distance: '200px'
+
+    });  
+    
+    
+
+    
